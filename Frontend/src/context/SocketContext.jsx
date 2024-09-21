fdfdc65ab5f78b5936872dded480fee911c27303
Varun Dubey <varun07dubey@gmail.com>
2024-09-21 17:58:33 +0530

    undeployed

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://elfchat-app.onrender.com/", {
+			const socket = io("http://localhost:5500", {
 				query: {
 					userId: authUser._id,
 				},
